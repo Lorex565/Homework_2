d38c201a36c530cbb65c53679774de0275dbec32
Іван Косьміна <ivan.kosm345@gmail.com>
2024-05-25 00:07:26 +0300

    Добавьте файлы проекта.

---
+++ Program1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter number number 1:");
+string num1 = Console.ReadLine();
+Console.WriteLine("Enter number number 2:");
+string num2 = Console.ReadLine();
+Console.WriteLine("Enter number number 3:");
+string num3 = Console.ReadLine();
+Console.WriteLine("Enter number number 4:");
+string num4 = Console.ReadLine();
+Console.WriteLine($"Number:{num1}{num2}{num3}{num4}");
---
+++ Program2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter a six - digit number:");
+int number = Convert.ToInt32(Console.ReadLine());
+int num1 = number / 100000;
+int num2 = number % 100000 / 10000;
+int num3 = number % 10000 / 1000;
+int num4 = number % 1000 / 100;
+int nym5 = number % 100 / 10;
+int num6 = number % 10;
+Console.WriteLine($"{num6}{nym5}{num4}{num3}{num2}{num1}");
